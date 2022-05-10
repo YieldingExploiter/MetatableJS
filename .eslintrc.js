fff31c7237b4d73d774397fdb8cc8a80e16f2ebf
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-05-11 00:45:14 +0200

    chore: :wrench: Migrate to eslintrc.json

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable object-curly-newline */
-module.exports = {
-  'extends': ['@norathefurry'], // use @norathefurry/base if you're dealing with vanilla JS
-  'rules': {
-    // your custom rules here
-  }
-};
