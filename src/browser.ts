6bc47c76d19ee568ec0d7dfbd3aa0ed1a094d33a
YieldingExploiter <yieldingexploiter@users.noreply.github.com>
2022-05-11 00:37:59 +0200

    fix: :bug: Fix typescript defs

--- src/browser.ts
+++ src/browser.ts
@@ -1,5 +1,6 @@
 // Basic Browser support.
 // Please just use a bundler and import index.ts instead of this file.
+// @ts-nocheck
 (()=>{
   const metatablejs = require('./index');
   window.metatablejs = metatablejs;
